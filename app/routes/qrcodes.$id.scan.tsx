191fbaa02087ad10d4fe4f02cb57f5519c83a4ba
phg-pih <phg.pih@gmail.com>
2025-04-23 10:24:05 +0700

    add scan QRCode route

---
+++ app/routes/qrcodes.$id.scan.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import db from "../db.server";
+import { getDestinationUrl } from "../models/QRCode.server";
+import type {QRCode} from "../models/QRCode.server";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.id, "Could not find QR code destination");
+
+  const id = parseFloat(params.id);
+
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  await db.qRCode.update({
+    where: { id },
+    data: { scans: { increment: 1 } },
+  });
+
+  return redirect(getDestinationUrl(qrCode as QRCode));
+}
