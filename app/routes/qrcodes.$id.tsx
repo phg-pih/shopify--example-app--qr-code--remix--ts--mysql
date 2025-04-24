8020fecec636bb8fa7ddb1cb84dad0f502fd384a
phg-pih <phg.pih@gmail.com>
2025-04-24 22:25:02 +0700

    add public route for QRCode

---
+++ app/routes/qrcodes.$id.tsx
@@ -0,0 +1,37 @@
+import {json, type LoaderFunctionArgs} from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { useLoaderData } from "@remix-run/react";
+import db from "../db.server";
+import {
+  getQRCodeImage,
+} from "../models/QRCode.server";
+
+interface QRCodePublic {
+  title: string;
+  image: string;
+}
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  invariant(params.id, "Could not find QR code destination");
+
+  const id = Number(params.id);
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  return json({
+    title: qrCode.title,
+    image: await getQRCodeImage(id),
+  } as QRCodePublic);
+};
+
+export default function QRCode() {
+  const { image, title } = useLoaderData() as QRCodePublic;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <img src={image} alt={`QR Code for product`} />
+    </>
+  );
+}
